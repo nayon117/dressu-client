4efd463fdf27572bceea5ac49c2cb50ef51ea158
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-27 08:09:44 +0600

    implement myclass page functionality in teacher route

--- src/pages/Dashboard/Teacher/Myclass.jsx
+++ src/pages/Dashboard/Teacher/Myclass.jsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react";
 
 
 const Myclass = () => {
+  const [myClasses, setMyClasses] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:5000/class-add/requests')
+    .then(res=>res.json())
+    .then(data=>setMyClasses(data))
+  },[])
   return (
-    <div>
-      <h2>This is Myclass</h2>
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Title</th>
+            <th>Image</th>
+            <th>Email</th>
+            <th>status</th>
+             
+          </tr>
+        </thead>
+        <tbody>
+          {myClasses.map((classItem, index) => (
+            <tr key={index}>
+              <th>{index + 1}</th>
+              <td>{classItem.title}</td>
+              <td>{classItem.image}</td>
+              <td>{classItem.email}</td>
+              <td>
+                <button>{classItem.status}</button>
+                
+              </td>
+              
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
